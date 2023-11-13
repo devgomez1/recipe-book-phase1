b2f538c6a30281baae76a34842496e6023cdd336
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-11-13 09:46:49 -0600

    small changes

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-fetch("www.themealdb.com/api/json/v1/1/categories.php")
-    .then(res => res.json())
-    .then(data => {
-        for (let category of data) {
-            renderList(category);
-        }
-    })
 
-function renderList(categoryObject) {
-    
-}
