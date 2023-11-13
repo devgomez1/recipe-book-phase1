c1dc1f69cc6140e7a55b04f22210f55604a7c896
Christopher <chrisjwkim97@gmail.com>
2023-11-13 10:36:04 -0500

    Button

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-fetch("www.themealdb.com/api/json/v1/1/categories.php")
-    .then(res => res.json())
-    .then(data => {
-        for (let category of data) {
-            renderList(category);
-        }
-    })
+let button = document.querySelector("#mealsearch");
+button.addEventListener("submit", renderSearch);
 
-function renderList(categoryObject) {
+function renderSearch(e) {
+    e.preventDefault();
+    let search = toTitleCase(e.target["meal"].value);
     
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if (data.strMeal === search) {
+                console.log("Found");
+            }
+        })
+
+    button.reset();
+}
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
 }
