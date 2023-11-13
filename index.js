8752c8d676e6cf84b35d365162ae64eb81deba47
Christopher <chrisjwkim97@gmail.com>
2023-11-13 10:51:40 -0500

    Button Progress

--- index.js
+++ index.js
@@ -4,17 +4,45 @@ button.addEventListener("submit", renderSearch);
 function renderSearch(e) {
     e.preventDefault();
     let search = toTitleCase(e.target["meal"].value);
-    
+
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
         .then(res => res.json())
         .then(data => {
             console.log(data);
-            if (data.strMeal === search) {
-                console.log("Found");
+            if (data.meals && data.meals.length > 0) {
+                const firstMeal = data.meals[0];
+                if (firstMeal.strMeal === search) {
+                    //ADDING FOUND MEAL TO PAGE
+                    let allKeys = Object.keys(firstMeal);
+
+                    let foodTitle = document.querySelector("#searchname");
+                    foodTitle.textContent = search;
+
+                    for (let key of allKeys) {
+                        if (key) {
+                            let instruction = document.createElement("p");
+                            instruction.textContent = firstMeal[key];
+                            foodTitle.append(instruction);
+                        }
+                    }
+                    
+                
+
+                    //ADDING FOUND MEAL TO PAGE
+                } else {
+                    console.log("Not found");
+                }
+            } else {
+                console.log("No meals found");
             }
         })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        });
 
     button.reset();
+
+
 }
 
 function toTitleCase(str) {
